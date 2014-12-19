88ea38f65dd5167e3262f842297c33c9e1d24ac9
lassebe <lassebe@kth.se>
2014-12-19 14:30:17 +0100

    girighet skapar nu korrekta lösningar, men för långsamt

--- casting/cast.cpp
+++ casting/cast.cpp
@@ -9,24 +9,28 @@ using namespace std;
 
 void readInput();
 void greedyStuff();
+void printAssignment();
+void printRoles();
+void printConflict();
+void printSolution();
 void verifySolution();
 bool contains(vector<int> & , int);
 bool assign_divas(int);
 
 
-// List of roles each actor can play
+// List of actors each actor can play
 // index in outer list = actor
-vector<vector<int>> roles;
+vector<vector<int>> actors;
 
-// List of roles that appear in the same scene as role [i]
+// List of actors that appear in the same scene as role [i]
 // index in outer list = role i
 vector<vector<int>> conflictRoles;
 
 //index in outer vector is actor
-// inner vector contains which roles actor plays
+// inner vector contains which actors actor plays
 vector<vector<int>> assignment;
 
-// #roles, scenes, actors
+// #actors, scenes, actors
 // minimum 1, 1, 2
 int n, s, k;
 // We can use n-1 "superactors" who can play any role, but only ONE role at a time
@@ -43,14 +47,18 @@ int main(){
 
   greedyStuff();
   //Simulated annealing
-  verifySolution();
+  
+  //For testing
+  //verifySolution();
+ // printConflict();
+  printSolution();
 }
 
 
-//Assign the divas, then give each actor as many roles as possible
+//Assign the divas, then give each actor as many actors as possible
 void greedyStuff(){
 
-  //Which roles have been assigned
+  //Which actors have been assigned
   bool assigned[n];
   for(int i=0;i<n;++i){
     assigned[i]=false;
@@ -65,13 +73,13 @@ void greedyStuff(){
   assigned[assignment[1][0]] = true;
 
   bool possible = true;
-  //Try to assign each actor as many roles as possible
-  for(int i=2; i < k; ++i){
-    //roles[i] = this actor can play these roles
-    for(auto possibleRole = roles[i].begin(); possibleRole != roles[i].end(); ++possibleRole){
+  //Try to assign each actor as many actors as possible
+  for(int actor=2; actor < k; ++actor){
+    //actors[i] = this actor can play these actors
+    for(auto possibleRole = actors[actor].begin(); possibleRole != actors[actor].end(); ++possibleRole){
       if(!assigned[*possibleRole] ){
-        for(auto it = assignment[i].begin(); it != assignment[i].end(); ++it){
-            if(contains(conflictRoles[*it],*possibleRole)){
+        for(auto it = conflictRoles[*possibleRole].begin(); it != conflictRoles[*possibleRole].end(); ++it){
+            if(contains(assignment[actor],*it)){
               possible = false;
             }
         }
@@ -81,17 +89,17 @@ void greedyStuff(){
 
       if(possible){
         assigned[*possibleRole] = true;
-        assignment[i].push_back(*possibleRole);
+        assignment[actor].push_back(*possibleRole);
       }
       possible = true;
     }
   }
 
   int superactor = k;
-  for(int i=0; i < n; ++i){
-    if(!assigned[i]){
-      assignment[i].push_back(superactor);
-      assigned[i] = true;
+  for(int role=0; role < n; ++role){
+    if(!assigned[role]){
+      assignment[superactor].push_back(role);
+      assigned[role] = true;
       ++superactor;
     }
   }
@@ -99,14 +107,12 @@ void greedyStuff(){
 }
 
 bool assign_divas(int i){
-  int diva1roles = roles[0].size();
-  int diva2roles = roles[1].size();
   int j=0;
-  while(i < diva1roles && j < diva2roles){
-    if(roles[0][i] != roles[1][j]){
-      if(!contains(conflictRoles[0],roles[1][j])){
-        assignment[0].push_back(roles[0][i]);
-        assignment[1].push_back(roles[1][j]);
+  while(i <  actors[0].size() && j < actors[1].size()){
+    if(actors[0][i] != actors[1][j]){
+      if(!contains(conflictRoles[0],actors[1][j])){
+        assignment[0].push_back(actors[0][i]);
+        assignment[1].push_back(actors[1][j]);
         return true;
       }
     }
@@ -125,33 +131,46 @@ bool contains(vector<int> & vec, int role){
 }
 
 
+void printSolution(){
+  int usedActors = 0;
+  for(auto it=assignment.begin(); it!=assignment.end(); ++it ){
+    if(it->size() > 0){
+      ++usedActors;
+    }
+  }
 
-
-
-
+  cout << usedActors << "\n";
+  int actorNumber=0;
+  for(auto it=assignment.begin(); it!=assignment.end(); ++it ){
+    if(it->size() > 0){
+      cout << actorNumber+1 << " ";
+      cout << it->size() << " ";
+      for(auto it2 = it->begin(); it2 != it->end(); ++it2){
+        cout << *it2+1 << " ";
+      }
+      cout << "\n";
+    }
+    ++actorNumber;
+  }  
+}
 
 
 void verifySolution(){
   //the divas have been casted (linear)
+
   assert(assignment[0].size() > 0);
   assert(assignment[1].size() > 0);
   //Check that the divas don't meet (constant)
   for(auto it = assignment[0].begin(); it != assignment[0].end(); ++it){
     for(auto it2 = assignment[1].begin(); it2 != assignment[1].end(); ++it2){
-      for(auto wat=conflictRoles[*it].begin(); wat != conflictRoles[*it].end(); ++wat){
-        cout << *wat << " ";
-      }
-      cout << "\n";
       assert(!contains(conflictRoles[*it],*it2));
     }
   }
-  //Check that no actor has conflict between roles (quadratic)
-  for(int i=0; i < n+k; ++i){
+  //Check that no actor has conflict between actors (quadratic)
+  for(int i=0; i < k+n; ++i){
     for(auto it = assignment[i].begin(); it != assignment[i].end(); ++it){
       for(auto it2 = assignment[i].begin(); it2 != assignment[i].end(); ++it2){
         if(*it != *it2){
-          cout  << "skådespelare= " << i << " r1=" << *it << " OCH r2=" << *it2 << "\n";
-          
           assert(!contains(conflictRoles[*it],*it2));
         }
       }
@@ -163,30 +182,24 @@ void verifySolution(){
 void readInput(){
   cin >> n >> s >> k;
 
-  roles = vector<vector<int>>(n,vector<int>(0));
+  actors = vector<vector<int>>(k,vector<int>(0));
   conflictRoles = vector<vector<int>>(n,vector<int>(0));
   assignment = vector<vector<int>>(k+n,vector<int>(0));
   //read actors for each role
   for(int i=0; i < n; ++i){      
-    int numRolesForActor=0;
-    cin >> numRolesForActor;
+    int numActorsForRole=0;
+    cin >> numActorsForRole;
     
     int actor;
-    for(int j=0; j < numRolesForActor; ++j){
+    for(int j=0; j < numActorsForRole; ++j){
       cin >> actor;
       //OFFSET BECAUSE VIGGO ÄR GALEN
-      roles[i].push_back(actor-1);
+      actors[actor-1].push_back(i);
+      
     }
   }
-  /* PRINT
-  for(int i=0; i < n; ++i){
-    for(auto it=roles[i].begin(); it != roles[i].end(); ++it){
-      cout << *it << " ";
-    }
-    cout << "\n";
-  }*/
 
-  //Read roles for each scene
+  //Read actors for each scene
   for(int i=0; i < s; ++i){
     int numRolesInScene=0;
     cin >> numRolesInScene;
@@ -200,7 +213,7 @@ void readInput(){
 
 
     //For each role that appears in the scene,
-    //add all other roles in the scene to conflict list
+    //add all other actors in the scene to conflict list
     for(int j=0; j < numRolesInScene; ++j){
       for(int k=0; k < numRolesInScene; ++k){
         if(k!=j && !contains(conflictRoles[temp[j]],temp[k]))
@@ -210,14 +223,53 @@ void readInput(){
   }
 
 
-  cout << "MOAR DEBUG ____________________\n";
+}
+
+void printAssignment(){
+  cout << "________________\n";
+  cout << "PRINTING ASSIGNMENT\n";
+  int i=0;
+  for(auto it=assignment.begin(); it != assignment.end();++it){
+    cout << i << "=";
+    for(auto it2=it->begin(); it2 != it->end(); ++it2){
+        cout << *it2 << " ";
+    }
+    ++i;
+    cout <<"\n";
+  }
+  cout << "FINISHED PRINTING ASSIGNMENT\n";
+  cout << "________________\n";
+}
+
+void printRoles(){
+  cout << "________________\n";
+  cout << "PRINTING ROLES\n";
+  int i=0;
+  for(auto it=actors.begin(); it != actors.end();++it){
+    cout <<"actor "<< i << "=";
+    for(auto it2=it->begin(); it2 != it->end(); ++it2){
+        cout << *it2 << " ";
+    }
+    ++i;
+    cout <<"\n";
+  }
+  cout << "FINISHED PRINTING ROLES\n";
+  cout << "________________\n";
+}
+
+
+void printConflict(){
+  cout << "________________\n";
+  cout << "PRINTING CONFLICT\n";
   int offset=0;
   for(auto it=conflictRoles.begin(); it != conflictRoles.end(); ++it){
-    cout << "roll=" << offset << " krockar med: ";
+    cout << "roll=" << offset << " conf with: ";
     for(auto wat= it->begin(); wat != it->end(); ++wat){
       cout << *wat << " ";
     }
     cout << "\n";
     ++offset;
   }
+  cout << "FINISHED PRINTING CONFLICT\n";
+  cout << "________________\n";
 }
