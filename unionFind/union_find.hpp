73cf737d2e2d20a29948cae28192204aa3d21f72
Lasse Berglund <lassebe@kth.se>
2015-01-09 22:22:03 +0100

    Create union_find.hpp
    Based on suprdewds version.

---
+++ unionFind/union_find.hpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+struct union_find {
+public:
+  union_find(const int n){
+    uf = std::vector<int> uf(n, -1);
+  }
+
+  bool unify(int x, int y){
+    int x_root = find(x);
+    int y_root = find(y);
+    if(x_root == y_root){
+      return false;
+    }
+    if(uf[x_root] < uf[y_root]){
+      uf[x_root] += uf[y_root];
+      uf[y_root] = x_root;
+    }else{
+      uf[y_root] += uf[x_root];
+      uf[x_root] = y_root;
+    }
+    return true;
+  }
+
+  int find(int x){
+    if(uf[x] < 0){
+      return x
+    }else{
+      uf[x] = find(uf[x]);
+    }
+  }
+
+private:
+  std::vector<int> uf;
+};
