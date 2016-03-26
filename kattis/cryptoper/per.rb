12a134cafd45ad5dd796db3f82c99a6e4889c5c3
lassebe <lassebe@kth.se>
2016-03-26 17:47:23 +0900

    changed to a stupid one-liner

--- kattis/cryptoper/per.rb
+++ kattis/cryptoper/per.rb
@@ -1,18 +1,1 @@
-# Asserts strings are of equal length 
-def count_diff str_a, str_b
-  count = 0
-  (0..str_a.length).each do |i|
-    count = count + 1 if str_a[i] != str_b[i]
-  end
-  count
-end
-
-line = STDIN.gets
-count = 0
-length = line.length
-(0..length-3).step(3).each do |i|
-  count = count + count_diff(line[i,3],"PER")
-end
-
-puts count
-
+puts STDIN.gets.scan(/.../).map { |str| str.chars.zip("PER".chars).reject { |c1,c2| c1 == c2 }.length }.reduce(:+)
