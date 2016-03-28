7cab56e067b4f010d374e25fb01a9aa091b0fb9a
lassebe <lassebe@kth.se>
2016-03-28 22:38:26 +0900

    not very ruby-like

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,29 +1,40 @@
-while true
-  n = STDIN.gets.to_i
-  break if n == 0
-  first_list = []
-  second_list = []
-  hash = {}
-  (0...n).each do |x|
-    first_list << STDIN.gets.to_i
+n = STDIN.gets.to_i
+
+
+def encrypt str
+  k = 1
+  (k..100).each do |i|
+    if i*i >= str.length
+      k = i
+      break
+    end
   end
 
-  (0...n).each do |x|
-    second_list << STDIN.gets.to_i
+  matrix = []
+  (0...k).each do |x|
+    arr = []
+    (0...k).each do |y|
+      if (x*k)+y < str.length
+        arr << str[(x*k)+y]
+      else
+        arr << "*"
+      end
+    end
+    matrix << arr
   end
-
-  sorted_list = first_list.sort
-  second_list.sort!
-  sorted_list.each_with_index do |x,index|
-    hash[x] = second_list[index]
+  
+  matrix.transpose.map(&:reverse).each do |line|
+    (0...k).each do |y|
+      if line[y] != "*"
+        print line[y]
+      end
+    end
   end
-
-
-  res = []
-  first_list.each do |x|
-    res << hash[x]
-  end
-
-  puts res
   puts
 end
+
+(0...n).each do |i|
+  line = STDIN.gets.strip
+  encrypt line
+end
+    
