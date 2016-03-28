fbeb3f86392d6df104322e8bc697607104772a92
lassebe <lassebe@kth.se>
2016-03-28 21:31:13 +0900

    another day another triv

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,14 +1,29 @@
-input = STDIN.gets.split(" ").map(&:to_i)
+while true
+  n = STDIN.gets.to_i
+  break if n == 0
+  first_list = []
+  second_list = []
+  hash = {}
+  (0...n).each do |x|
+    first_list << STDIN.gets.to_i
+  end
 
-n = input[0]
-b = input[1]
-max = 0
-(0..b).each do |i|
-  max = max + 2**i
-end
+  (0...n).each do |x|
+    second_list << STDIN.gets.to_i
+  end
 
-if n <= max 
-  puts "yes"
-else
-  puts "no"
+  sorted_list = first_list.sort
+  second_list.sort!
+  sorted_list.each_with_index do |x,index|
+    hash[x] = second_list[index]
+  end
+
+
+  res = []
+  first_list.each do |x|
+    res << hash[x]
+  end
+
+  puts res
+  puts
 end
