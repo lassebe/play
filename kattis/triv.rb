f7e3ed7d4bdd2614fda260bc40f26602599e80c7
lassebe <lassebe@kth.se>
2016-03-30 20:56:58 +0900

    tick tick tick boom!

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,32 +1,55 @@
-dogs = STDIN.gets.split(" ").map(&:to_i)
-dog_a = []
-dog_b = []
-(0...200).each do |i|
-  (0...dogs[0]).each do
-    dog_a << true
-  end
-  (0...dogs[1]).each do 
-    dog_a << false
-  end
-  
-  (0...dogs[2]).each do
-    dog_b << true
-  end
-  (0...dogs[3]).each do 
-    dog_b << false
-  end
 
+def digitize str
+  if str =="**** ** ** ****"
+    0
+  elsif str =="  *  *  *  *  *"
+    1
+  elsif str =="***  *****  ***"
+    2
+  elsif str =="***  ****  ****"
+    3
+  elsif str =="* ** ****  *  *"
+    4
+  elsif str =="****  ***  ****"
+    5
+  elsif str =="****  **** ****"
+    6  
+  elsif str =="***  *  *  *  *"
+    7
+  elsif str =="**** ***** ****"
+    8
+  elsif str =="**** ****  ****"
+    9
+  else
+    -1
+  end
 end
 
-arrival = STDIN.gets.split(" ").map(&:to_i)
 
-arrival.each do |arr|
-  if dog_a[arr-1] and dog_b[arr-1]
-    puts "both"
-  elsif dog_a[arr-1] or dog_b[arr-1]
-    puts "one"
-  else
-    puts "none"
+input = STDIN.readlines
+(0...input.size-1).each {|i| input[i].delete!("\n")}
+digits = (input[0].size+1) / 4
+
+
+arr = []
+
+(0...digits).each do |digit|
+  str = ""
+  (0...5).each do |row|
+    str << input[row][4*digit,3]
   end
-      
+  d = digitize str
+  if d == -1
+    arr = []
+    break
+  end
+  arr << d
+end
+
+if arr.empty?
+  puts "BOOM!!"
+elsif arr[-1] % 2 == 0 and arr.reduce(:+) % 3 == 0
+  puts "BEER!!"
+else
+  puts "BOOM!!"
 end
