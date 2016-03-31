a3db0ef995c5fa46ed4f7edfd1eb6f2fbbe2a33d
lassebe <lassebe@kth.se>
2016-03-31 17:29:05 +0900

    not actual insertionsort

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,17 +1,27 @@
-lines = STDIN.readlines.map(&:strip)
+def insertion_sort arr
+  steps = 0
+  sorted = [arr[0]]
+  (1...arr.size).each do |i|
+    #p sorted
+    sorted.each_with_index do |el,index|
+      next if arr[i] >= el
 
-lines.each do |line|
-  name = []
-  pulse = []
-  line.split(" ").each do |word|
-    w = word.to_f
-    if w == 0.0 and word != "0.0"
-      name << word
-    else
-      pulse << w
+      steps = steps + (sorted.size-index)
+      sorted.insert(index,arr[i])
+      break
     end
+    sorted = sorted.push(arr[i]) if sorted.size != i+1
+    #p sorted
+   # puts steps
   end
-  average = (pulse.reduce(:+) / pulse.size)
-  print '%.6f' % average
-  puts " #{name.join(" ")}"
+  steps
+end
+
+
+tests = STDIN.gets.to_i
+
+(0...tests).each do |t|
+  line = STDIN.gets.split(" ").map(&:to_i)[1..-1]
+#  p line
+  puts "#{t+1} #{insertion_sort line}"
 end
