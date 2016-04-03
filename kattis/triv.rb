97dd53f88a537f11efb5edc33d42496c2a2187d0
Lasse Berglund <lassebe@kth.se>
2016-04-03 23:39:03 +0900

    Clear triv.rb

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,17 +1,1 @@
-tests = STDIN.gets.to_i
 
-(0...tests).each do |test|
-  turtles = STDIN.gets.split(" ").map(&:to_i)
-
-  lower_bound = 0
-  prev = turtles[0]
-  (1...turtles.size).each do |turtle|
-    if turtles[turtle] > 2*prev
-      lower_bound = lower_bound + (turtles[turtle] - 2*prev)
-    end
-
-    prev = turtles[turtle]
-  end
-
-  puts lower_bound
-end
