c220d8988eff39dfa291ec4409a7b466a3a5b9fb
Lasse Berglund <lassebe@kth.se>
2016-04-07 22:53:09 +0900

    Update triv.rb
    Written from phone, correct on first try, if we discount encoding issues from converting from evernote to dropbox

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,24 +1,19 @@
-input = STDIN.gets.split(" ")
-
-h = input[0].to_i
-
-root = 2**(h+1)-1
-if input.size == 1
-  puts root
-  exit(0)
-end
-
-
-line = input[1]
-
-i = 1
-line.chars.each do |c|
-  if c == "L"
-    i = i * 2
-  else
-    i = i * 2 + 1
+ws = STDIN.readlines.map(&:strip)
+words = []
+ws.each do |w|
+  w.split(" ").each do |v|
+    words << v
   end
 end
 
+compound = {}
+words.each do |w1|
+  words.each do |w2|
+    next if w1 == w2
+    compound[w1+w2] = 1
+  end
+end
 
-puts root-(i-1)
+compound.keys.sort.each do |c|
+  puts c
+end
