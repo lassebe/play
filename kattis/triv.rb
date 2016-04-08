5677f4ff7a125bcf0c7cbda7bebf8ba5a95fd6c3
lassebe <lassebe@kth.se>
2016-04-08 18:14:00 +0900

    probably could've been done more elegantly

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,24 +1,54 @@
-input = STDIN.gets.split(" ")
-
-h = input[0].to_i
-
-root = 2**(h+1)-1
-if input.size == 1
-  puts root
-  exit(0)
+n = STDIN.gets.to_i
+line = STDIN.gets.strip
+adrian = []
+bruno = []
+goran = []
+while true 
+  adrian << "A" 
+  break if adrian.size == n
+  adrian << "B"
+  break if adrian.size == n
+  adrian << "C"
+  break if adrian.size == n
+end
+while true 
+  bruno << "B" 
+  break if bruno.size == n
+  bruno << "A"
+  break if bruno.size == n
+  bruno << "B"
+  break if bruno.size == n
+  bruno << "C"
+  break if bruno.size == n
 end
 
+while true 
+  goran << "C" 
+  break if goran.size == n
+  goran << "C" 
+  break if goran.size == n
+  goran << "A"
+  break if goran.size == n
+  goran << "A"
+  break if goran.size == n
+  goran << "B"
+  break if goran.size == n
+  goran << "B"
+  break if goran.size == n
+end
+adrian = adrian.join("")
+bruno = bruno.join("")
+goran = goran.join("")
 
-line = input[1]
-
-i = 1
-line.chars.each do |c|
-  if c == "L"
-    i = i * 2
-  else
-    i = i * 2 + 1
-  end
+scores = {"Adrian" => 0, "Bruno" => 0, "Goran" => 0}
+(0...n).each do |i|
+  scores["Adrian"] = scores["Adrian"] + 1 if adrian[i] == line[i]
+  scores["Bruno"] = scores["Bruno"] + 1 if bruno[i] == line[i]
+  scores["Goran"] = scores["Goran"] + 1 if goran[i] == line[i]
 end
 
-
-puts root-(i-1)
+max = scores.values.sort[-1]
+puts max
+scores.keys.sort.each do |key|
+  puts key if scores[key] == max
+end
