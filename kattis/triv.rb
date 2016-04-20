5a7c01d2739a8ea69fc73dc0f0a8a683645a2e44
lassebe <lassebe@kth.se>
2016-04-20 21:33:39 +0900

    another day another kattis problem

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,34 +1,12 @@
-while true
-  room = STDIN.gets.strip.split(" ").map(&:to_i)
-  break if room == [0,0]
-  dirs = STDIN.gets.to_i
-  x,actual_x = 0,0
-  y,actual_y = 0,0
-  
-  (0...dirs).each do |d|
-    walk = STDIN.gets.strip.split(" ")
+lines = STDIN.readlines.map(&:strip)
 
-    length = walk.last.to_i
+regex = /.*problem.*/
 
-    case walk.first
-    when "r"
-      x += length
-      actual_x = (actual_x + length) < room.first ? (actual_x + length) : room.first-1
-    when "l"
-      x -= length
-      actual_x = (actual_x - length) >= 0 ? (actual_x - length) : 0
-    when "d"
-      y -= length
-      actual_y = (actual_y - length) >= 0 ? (actual_y - length) : 0
-    when "u"
-      y += length
-      actual_y = (actual_y + length) < room.last ? (actual_y + length) : room.last-1
-    else
-      puts "wat"
-    end
+lines.each do |line|
+  line.downcase!
+  if line.match(regex)
+    puts "yes"
+  else
+    puts "no"
   end
-
-  puts "Robot thinks #{x} #{y}"
-  puts "Actually at #{actual_x} #{actual_y}"
-  puts
 end
