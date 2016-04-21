6d7593cfcb661216c8468bccbefe13ac7ec1d538
lassebe <lassebe@kth.se>
2016-04-21 14:24:48 +0900

    remember, ruby returns the last statement of the function

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,12 +1,27 @@
 lines = STDIN.readlines.map(&:strip)
 
-regex = /.*problem.*/
+unique_names = Hash.new(0)
 
 lines.each do |line|
-  line.downcase!
-  if line.match(regex)
-    puts "yes"
+  first_name = line.split(" ").first
+  last_name = line.split(" ").last
+  unique_names[first_name] += 1
+end
+
+lines.sort! do |name_a,name_b|
+  name_a = name_a.split(" ")
+  name_b = name_b.split(" ")
+  if name_a.last == name_b.last 
+    name_a.first <=> name_b.first 
   else
-    puts "no"
+    name_a.last <=> name_b.last
+  end
+end
+
+lines.each do |line|
+  if unique_names[line.split(" ").first] > 1
+    puts line
+  else
+    puts line.split(" ").first
   end
 end
