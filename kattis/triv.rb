f70d5ad3a4a127ea927d2018e1a502e02c6d9b3b
lassebe <lassebe@kth.se>
2016-04-22 18:59:02 +0900

    throwback till NCPC

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,15 +1,38 @@
 input = STDIN.gets.strip.split(" ").map(&:to_i)
 
-n = input[0]
-p = input[1]
-q = input[2]
+c = input.first
+n = input.last
 
+consistent = true
+on_train = 0
+(0...n).each do |i|
+  station = STDIN.gets.strip.split(" ").map(&:to_i)
+  left = station[0]
+  enter = station[1]
+  wait = station[2]
+  
+  if on_train - left < 0 
+    consistent = false
+    break
+  end
+  
+  on_train -= left
+  
+  if on_train + enter > c
+    consistent = false
+    break
+  end
+  
+  on_train += enter
 
-rounds = p+q
+  if wait > 0 and on_train < c
+    consistent = false
+    break
+  end
+end
 
-
-if (rounds/n) % 2 == 0
-  puts "paul"
+if consistent and on_train == 0
+  puts "possible"
 else
-  puts "opponent"
+  puts "impossible"
 end
