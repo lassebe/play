3d4a48e58a6d812a720ea38254a69519f44ee5e9
lassebe <lassebe@kth.se>
2016-05-01 19:03:49 +0900

    counting the days

--- kattis/triv.rb
+++ kattis/triv.rb
@@ -1,18 +1,16 @@
-line = STDIN.gets.strip.split(" ").map(&:to_i)
-a = line.first
-b = line.last
+tests = STDIN.gets.to_i
+(0...tests).each do |t|
+  number_of_months = STDIN.gets.strip.split(" ")[1].to_i
+  months = STDIN.gets.strip.split(" ").map(&:to_i)
 
-prng = Random.new
-alphabet = ("a"..."z").to_a
-
-size = 15
-essay = []
-(0...b).each do |w|
-  word = []
-  (0...size).each do |letter|
-    word << alphabet[prng.rand(25)]
+  lucky_days = 0
+  day_of_week = 0
+  months.each_with_index do |month,index|
+    (0...month).each do |day_of_month|
+      lucky_days += 1 if day_of_week == 5 and day_of_month == 12
+      day_of_week = (day_of_week + 1) % 7
+    end
   end
-  essay << word.join("")
-end
 
-puts essay.join(" ")
+  puts lucky_days
+end
