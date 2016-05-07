713cf036f084964428ef0a351957db1a1c8c8d2d
lassebe <lassebe@kth.se>
2016-05-07 22:56:36 +0900

    moving course stuff here for easy access

---
+++ opres/result.sol
@@ -0,0 +1,5 @@
+solution status: optimal solution found
+objective value:                   0.0705109100585418
+x                                 0.00952634379989356 	(obj:0.75)
+y                                  0.0382650345928685 	(obj:0.5)
+z                                   0.294890899414582 	(obj:0.15)
